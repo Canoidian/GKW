2b4b32357f8958c2a3f08775d3e9f3a7d80c0b08
William Isaak <eh.canoidian@gmail.com>
2023-12-18 10:47:47 -0500

    Co-authored-by: galecoz <galecoz@users.noreply.github.com> Co-authored-by: KevinNhan123 <KevinNhan123@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 #    By: Kevin Nhan <kenha4996@ugcloud.ca>            +#+                      #
 #                                                    +#+                       #
 #    Created: 2023/12/13 11:07:56 by williamisaa   #+#    #+#                  #
-#    Updated: 2023/12/14 09:08:33 by williamisaa   ########   odam.nl          #
+#    Updated: 2023/12/18 10:47:30 by williamisaa   ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,7 +23,6 @@ from settings import *
 
 #* Constants and Variables
 
-
 #Colours
 BLACK = (0,0,0)
 WHITE = (255,255,255)
@@ -34,19 +33,31 @@ RED = (255,0,0)
 done = False
  
 pygame.init() #Initializes the window
+pygame.font.init()
 
 # Creating window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("12/25") # Name of game
 clock = pygame.time.Clock()
 
+class character(pygame.sprite.Sprite):
+    def __init__(self, speed, gifts, buffs, name, image, pos):
+        super().__init__()
+        self.speed = speed
+        self.gifts = gifts
+        self.buffs = buffs
+        self.name = name
+        self.image = pygame.image.load("").convert_alpha()
+        self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+        
+#player = Player()
+
 # Load images - background ect
-# background = pygame.transform.scale(pygame.image.load("").convert(), (WIDTH, HEIGHT)) # Input path to background file
+background = pygame.transform.scale(pygame.image.load("/Users/williamisaak/Code/GKW/Asset/Background/Background.jpg").convert(), (WIDTH, HEIGHT)) # Input path to background file
 
 # #Draws a red rectangle at the coords (100,100) on the window
 pygame.draw.rect(screen, WHITE, [100,100,150,200])
 pygame.display.flip()
-
 
 
 while True:
@@ -57,7 +68,12 @@ while True:
             pygame.quit()
             exit()
     
-    #screen.blit(background, (0,0)) # To display background
+    # Detects when user holds W key
+    if keys[pygame.K_w]:
+        print("Holding W key")
+    
+    screen.blit(background, (0,0)) # To display background
+    #screen.blit(player.image, player.pos)
     
     pygame.display.update()
     clock.tick(FPS)
--- settings.py
+++ settings.py
@@ -1,6 +1,18 @@
 # Game setup
 
 WIDTH = 1280
-HEIGHT = 720
+HEIGHT = 1280
 
 FPS = 60
+
+#Character Settings
+
+PLAYER_START_X = 400
+PLAYER_START_Y = 500
+
+speed = 0
+gifts = 0
+buffs = 0
+name = ""
+image = ""
+
